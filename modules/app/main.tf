90f692684b6388bdb847cb265855f6deb54ad7b1
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-24 21:28:43 +0530

    repo name changed

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -1,47 +1,47 @@
-# create an instance
-resource "aws_instance" "instance" {
-  ami = data.aws_ami.ami.image_id
-  instance_type = var.instance_type
-  instance_market_options {
-    market_type = "spot"
-    spot_options {
-      instance_interruption_behavior = "stop"
-      spot_instance_type             = "persistent"
-    }
-  }
-  tags = {
-    Name = "${var.env}-${var.component}-demo"
-    monitor = "yes"
-
-  }
-}
-# # create a security group
-# resource "aws_security_group" "security_group" {
-#   name        = "${var.env}-sg"
-#   ingress {
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#
-#   }
-#   egress {
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#
+# # create an instance
+# resource "aws_instance" "instance" {
+#   ami = data.aws_ami.ami.image_id
+#   instance_type = var.instance_type
+#   instance_market_options {
+#     market_type = "spot"
+#     spot_options {
+#       instance_interruption_behavior = "stop"
+#       spot_instance_type             = "persistent"
+#     }
 #   }
 #   tags = {
-#     Name = "${var.env}-sg"
+#     Name = "${var.env}-${var.component}-demo"
+#     monitor = "yes"
+#
 #   }
 # }
+# # # create a security group
+# # resource "aws_security_group" "security_group" {
+# #   name        = "${var.env}-sg"
+# #   ingress {
+# #     from_port        = 0
+# #     to_port          = 0
+# #     protocol         = "-1"
+# #     cidr_blocks      = ["0.0.0.0/0"]
+# #
+# #   }
+# #   egress {
+# #     from_port        = 0
+# #     to_port          = 0
+# #     protocol         = "-1"
+# #     cidr_blocks      = ["0.0.0.0/0"]
+# #
+# #   }
+# #   tags = {
+# #     Name = "${var.env}-sg"
+# #   }
+# # }
 resource "null_resource" "null_instance" {
   connection {
     type     = "ssh"
     user     = "ec2-user"
     password = "DevOps321"
-    host     = aws_instance.instance.public_ip
+    host     = 23.20.101.146
   }
   provisioner "remote-exec" {
     inline = [
@@ -50,12 +50,12 @@ resource "null_resource" "null_instance" {
     ]
   }
 }
-resource "aws_route53_record" "record" {
-  name      = "${var.component}-${var.env}"
-  type      = "A"
-  zone_id   = var.zone_id
-  ttl       = 5
-  records = [aws_instance.instance.private_ip]
-}
+# resource "aws_route53_record" "record" {
+#   name      = "${var.component}-${var.env}"
+#   type      = "A"
+#   zone_id   = var.zone_id
+#   ttl       = 5
+#   records = [aws_instance.instance.private_ip]
+# }
 
 
