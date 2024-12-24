d722aef00fcb1e97e0c185bfc7e98ae3070b5eeb
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-24 17:06:24 +0530

    repo name changed

--- main.tf
+++ main.tf
@@ -7,14 +7,14 @@ module "frontend" {
   zone_id     = var.zone_id
 }
 module "backend" {
-  depends_on = [module.db]
+  depends_on = [module.mysql]
   source = "./modules/app"
   component = "backend"
   instance_type=var.instance_type
   env = var.env
   zone_id = var.zone_id
 }
-module "db" {
+module "mysql" {
   source = "./modules/app"
   component = "db"
   instance_type=var.instance_type
