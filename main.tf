96f87d99f237014dc6b5d721be5ed1ea960e8c61
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-30 20:45:23 +0530

    vault

--- main.tf
+++ main.tf
@@ -6,18 +6,18 @@ module "frontend" {
   env = var.env
   zone_id     = var.zone_id
 }
-module "backend" {
-#   depends_on = [module.mysql]
-  source = "./modules/app"
-  component = "backend"
-  instance_type=var.instance_type
-  env = var.env
-  zone_id = var.zone_id
-}
-module "mysql" {
-  source = "./modules/app"
-  component = "mysql"
-  instance_type=var.instance_type
-  env = var.env
-  zone_id = var.zone_id
-}
+# module "backend" {
+# #   depends_on = [module.mysql]
+#   source = "./modules/app"
+#   component = "backend"
+#   instance_type=var.instance_type
+#   env = var.env
+#   zone_id = var.zone_id
+# }
+# module "mysql" {
+#   source = "./modules/app"
+#   component = "mysql"
+#   instance_type=var.instance_type
+#   env = var.env
+#   zone_id = var.zone_id
+# }
