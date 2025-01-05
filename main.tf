91e435f8e67881c25855be82e9dda85b7a1b56c9
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-05 20:30:48 +0530

    backend

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-module "frontend" {
-  depends_on = [module.backend]
-  source = "./modules/app"
-  component = "frontend"
-  instance_type=var.instance_type
-  env = var.env
-  zone_id     = var.zone_id
-  vault_token = var.vault_token
-}
+# module "frontend" {
+#   depends_on = [module.backend]
+#   source = "./modules/app"
+#   component = "frontend"
+#   instance_type=var.instance_type
+#   env = var.env
+#   zone_id     = var.zone_id
+#   vault_token = var.vault_token
+# }
 module "backend" {
-  depends_on = [module.mysql]
+#   depends_on = [module.mysql]
   source = "./modules/app"
   component = "backend"
   instance_type=var.instance_type
@@ -16,11 +16,11 @@ module "backend" {
   zone_id = var.zone_id
   vault_token = var.vault_token
 }
-module "mysql" {
-  source = "./modules/app"
-  component = "mysql"
-  instance_type=var.instance_type
-  env = var.env
-  zone_id = var.zone_id
-  vault_token = var.vault_token
-}
+# module "mysql" {
+#   source = "./modules/app"
+#   component = "mysql"
+#   instance_type=var.instance_type
+#   env = var.env
+#   zone_id = var.zone_id
+#   vault_token = var.vault_token
+# }
