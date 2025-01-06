fa1ad13cfa7607ccc0d98740d37a1226541730c2
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-06 13:27:27 +0530

    host name changed

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-# module "frontend" {
-#   depends_on = [module.backend]
-#   source = "./modules/app"
-#   component = "frontend"
-#   instance_type=var.instance_type
-#   env = var.env
-#   zone_id     = var.zone_id
-#   vault_token = var.vault_token
-# }
+module "frontend" {
+  depends_on = [module.backend]
+  source = "./modules/app"
+  component = "frontend"
+  instance_type=var.instance_type
+  env = var.env
+  zone_id     = var.zone_id
+  vault_token = var.vault_token
+}
 module "backend" {
 #   depends_on = [module.mysql]
   source = "./modules/app"
