baa9ee02427f91948bfe733362c94fa7a400a731
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-06 13:38:09 +0530

    create mysql

--- main.tf
+++ main.tf
@@ -1,26 +1,26 @@
-module "frontend" {
-  depends_on = [module.backend]
-  source = "./modules/app"
-  component = "frontend"
-  instance_type=var.instance_type
-  env = var.env
-  zone_id     = var.zone_id
-  vault_token = var.vault_token
-}
-module "backend" {
-#   depends_on = [module.mysql]
-  source = "./modules/app"
-  component = "backend"
-  instance_type=var.instance_type
-  env = var.env
-  zone_id = var.zone_id
-  vault_token = var.vault_token
-}
-# module "mysql" {
+# module "frontend" {
+#   depends_on = [module.backend]
 #   source = "./modules/app"
-#   component = "mysql"
+#   component = "frontend"
+#   instance_type=var.instance_type
+#   env = var.env
+#   zone_id     = var.zone_id
+#   vault_token = var.vault_token
+# }
+# module "backend" {
+# #   depends_on = [module.mysql]
+#   source = "./modules/app"
+#   component = "backend"
 #   instance_type=var.instance_type
 #   env = var.env
 #   zone_id = var.zone_id
 #   vault_token = var.vault_token
 # }
+module "mysql" {
+  source = "./modules/app"
+  component = "mysql"
+  instance_type=var.instance_type
+  env = var.env
+  zone_id = var.zone_id
+  vault_token = var.vault_token
+}
