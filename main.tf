2e167281886f3d12abff85bcc8aad7b1731602ec
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-08 16:40:01 +0530

    create rds

--- main.tf
+++ main.tf
@@ -1,29 +1,29 @@
-module "frontend" {
-  depends_on = [module.backend]
-  source = "./modules/app"
-  component = "frontend"
-  instance_type=var.instance_type
-  env = var.env
-  zone_id     = var.zone_id
-  vault_token = var.vault_token
-}
-module "backend" {
-  depends_on = [module.mysql]
-  source = "./modules/app"
-  component = "backend"
-  instance_type=var.instance_type
-  env = var.env
-  zone_id = var.zone_id
-  vault_token = var.vault_token
-}
-module "mysql" {
-  source = "./modules/app"
-  component = "mysql"
-  instance_type=var.instance_type
-  env = var.env
-  zone_id = var.zone_id
-  vault_token = var.vault_token
-}
+# module "frontend" {
+#   depends_on = [module.backend]
+#   source = "./modules/app"
+#   component = "frontend"
+#   instance_type=var.instance_type
+#   env = var.env
+#   zone_id     = var.zone_id
+#   vault_token = var.vault_token
+# }
+# module "backend" {
+#   depends_on = [module.mysql]
+#   source = "./modules/app"
+#   component = "backend"
+#   instance_type=var.instance_type
+#   env = var.env
+#   zone_id = var.zone_id
+#   vault_token = var.vault_token
+# }
+# module "mysql" {
+#   source = "./modules/app"
+#   component = "mysql"
+#   instance_type=var.instance_type
+#   env = var.env
+#   zone_id = var.zone_id
+#   vault_token = var.vault_token
+# }
 module "rds"{
   source = "./modules/app"
   component = "rds"
@@ -31,5 +31,5 @@ module "rds"{
   instance_type = var.instance_type
   vault_token = var.vault_token
   zone_id = var.zone_id
-  app_port = 3306
+#   app_port = 3306
 }
